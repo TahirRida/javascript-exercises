7e5e3d62993366ca9a5a1f5ae6c9ba53eed3ba0b
TahirRida <tahirrida52@gmail.com>
2023-03-23 21:44:58 +0000

    answered the first two exercises, the tests passed successfully

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+function repeatString(stringSource,numberOfTimes) {
+    if (numberOfTimes === 0){
+        return '';
+    }
+    if (numberOfTimes < 0 ){
+        return 'ERROR';
+    }
+    let result=stringSource;
+    for (let i = 1;i<numberOfTimes;i++){
+        result+=stringSource;
+    }
+    return result;
 
 };
 
