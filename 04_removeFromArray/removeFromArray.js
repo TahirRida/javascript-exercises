cc81fb334629edfbf923b3250afae93bbd4f39a8
TahirRida <tahirrida52@gmail.com>
2023-03-24 15:37:10 +0000

    solved exercise number five

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for (let i = 0; i<arguments.length;i++){
+        if (array.includes(arguments[i])){
+            for (let j = 0; j<array.length;j++){
+                if (array[j] === arguments[i]){
+                    array.splice(j,1);
+                    break;
+                }
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
+    //the function should return ERROR when the given arguments are not numbers
+    if (!Number.isInteger(min) || !Number.isInteger(max) || isNaN(min)  || isNaN(max) || min<0 || max<0){
+        return 'ERROR' ;
+    }
+    //if the arguments are not in order (i.e the second argument is bigger than the first), we'll swap them
+    if (min > max){
+        const temp = max;
+        max = min;
+        min = temp;
+    }
+    let sum = 0;
+    for (let i = min;i<=max;i++){
+        sum += i;
+    }
+    return sum;
 
 };
 
